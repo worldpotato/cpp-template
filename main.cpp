9780b6521b864dae080039b4519d9f52f54938c8
Felix Strobel <mail@felixstrobel.de>
2021-09-17 13:41:57 +0200

    add template CMakeLists and main

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @author      : worldpotato
+ * @file        : main
+ * @created     : Sunday Dec 13, 2020 11:59:33 CET
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello World" << std::endl;
+    return 0;
+}
+
+
