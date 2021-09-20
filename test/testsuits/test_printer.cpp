497b631235741e1e58b8a2d185b14262ff7ef300
Felix Strobel <mail@felixstrobel.de>
2021-09-20 18:59:27 +0200

    add tests for printer

---
+++ test/testsuits/test_printer.cpp
@@ -0,0 +1,24 @@
+#include "gtest/gtest.h"
+#include "printer.hpp"
+
+TEST(printer, pre_defined_string)
+{
+    printer p = printer("test");
+    int ret = p.print();
+    ASSERT_EQ(ret, 0);
+}
+
+TEST(printer, no_pre_defined_string)
+{
+    printer p = printer();
+    int ret = p.print();
+    ASSERT_EQ(ret, 1);
+}
+
+TEST(printer, given_string)
+{
+    printer p = printer();
+    int ret = p.print("test");
+    ASSERT_EQ(ret, 0);
+}
+
